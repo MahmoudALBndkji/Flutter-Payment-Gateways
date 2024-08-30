8fee3c7e71da00e0dd040025de58e8ecf8c31407
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2024-08-30 06:51:15 +0300

    Create Ephemeral Key

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_payment_gateways/core/utils/api_keys.dart';
 import 'package:flutter_payment_gateways/core/utils/api_service.dart';
+import 'package:flutter_payment_gateways/features/checkout/data/models/ephermeral_key_model/ephermeral_key_model.dart';
 import 'package:flutter_payment_gateways/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:flutter_payment_gateways/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -41,5 +42,23 @@ class StripeService {
       paymentIntentClientSecret: paymentIntentModel.clientSecret!,
     );
     await displayPaymentSheet();
+  }
+
+  Future<EphermeralKeyModel> createEphemeralKey(
+      {required String customerId}) async {
+    var response = await apiService.post(
+      contentType: Headers.formUrlEncodedContentType,
+      body: {
+        "customer": customerId,
+      },
+      url: 'https://api.stripe.com/v1/ephemeral_keys',
+      token: ApiKeys.secrectKey,
+      headers: {
+        "Authorization": "Bearer ${ApiKeys.secrectKey}",
+        "Stripe-Version": "2024-06-20",
+      },
+    );
+    var ephermeralKeyModel = EphermeralKeyModel.fromJson(response.data);
+    return ephermeralKeyModel;
   }
 }
